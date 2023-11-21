d355229456d18c6725fc7691b1006782ca636cb7
AlexHutanu <valexhutanu@gmail.com>
2023-11-21 15:38:26 +0200

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Param } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+
+  @Get(":number")
+  plusOne(@Param() params: any): string {
+    let sum:number = parseInt(params.number) + 5
+    return `The number is ${sum}`;
+  }
+}
